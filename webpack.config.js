d48be924ae316f07929dc5db3eca227a257ecdd1
Xavier Lepretre <xavier.lepretre@gmail.com>
2018-02-09 01:09:43 +0000

    Added index.html to build process.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     entry: "./app/js/app.js",
     output: {
-        path: __dirname + "/build/app/js",
-        filename: "app.js"
+        path: __dirname + "/build/app",
+        filename: "js/app.js"
     },
+    plugins: [
+        new CopyWebpackPlugin([
+            // Copy our app's index.html to the build folder.
+            { from: './app/index.html', to: "index.html" },
+        ]),
+    ],
     module: {
         loaders: []
     }
